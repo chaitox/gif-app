a7f2419f944a2a1597a4ff36bc7121236fbc055a
ricardo <ricardo_jul_88@gmail.com>
2021-01-30 17:22:37 -0300

    segundo commit

--- src/componets/GifGrid.js
+++ src/componets/GifGrid.js
@@ -1,40 +1,23 @@
-import React, { useEffect, useState } from 'react'
+
+import { useFetchGif } from '../hooks/useFetchGif';
 
 export const GifGrid = ({ category }) => {
 
     
-    const [images, setImages] = useState([]);
 
 
-    useEffect(()=>{        
-        getGifs();
-    },[])
+  const {data, loading} = useFetchGif(category)
 
 
-    const getGifs = async ()=>{
-        const url = 'https://api.giphy.com/v1/gifs/search?api_key=PzUi8uRiVuf71mTgeoXlATKm3aHMhT1O&q=Rick and Morty&limit=10';
-        const resp = await fetch(url)
-        const { data } = await resp.json();
-
-        
-        const gifs = data.map( img =>{
-            return {
-                id: img.id,
-                title: img.title,
-                image:img.images?.downsized_medium.url
-            }
-        })
-        console.log(gifs);
-        setImages( gifs );
-    }
+    
 
 
     return (
-        <div>
+        <div className="card">
             <h3> {category} </h3>
             <ol>
                 {
-                    images.map(({title, image, id})=>{
+                    data.map(({title, image, id})=>{
                         return <>
                             <li key={id}>{title}</li>
                             <img src={image} alt='algo'/>
